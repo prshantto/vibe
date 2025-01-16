48d1d0418a23691fb9b495fd70f95566537a099e
Prashant <prashanttt001@gmail.com>
2025-01-16 12:09:29 +0530

    compleated login/signup pages

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,7 +1,128 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./Form.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const handleGoogleLogin = () => {
+    console.log("Google login clicked");
+  };
+
+  const handeleSubmit = (e) => {
+    e.preventDefault();
+    const user = {
+      username,
+      password,
+    };
+    axios
+      .post(`${import.meta.env.VITE_API_URL}/api/users/login`, user)
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          setErrors({});
+          navigate("/home");
+        }
+      })
+      .catch((err) => {
+        const errorMessage =
+          err.response?.data?.errors?.[0]?.msg ||
+          err.response?.data?.message ||
+          "An error occurred during login";
+        console.log(err);
+        setErrors({ error: errorMessage });
+      });
+    setUsername("");
+    setPassword("");
+  };
+  return (
+    <div className="container-body">
+      <div className="container">
+        <div className="form-header">
+          <h2>
+            Login to <span className="text-[#85def8]">Vibe</span>
+            <span className="text-[#9de5b8]">Chat</span>
+            <img
+              className="w-8 h-8 ml-3 inline-block"
+              src="./chat.png"
+              alt=""
+            />
+            <p className="welcome-message text-sm">
+              Welcome! Please fill in the details to get started.
+            </p>
+          </h2>
+          <form onSubmit={handeleSubmit}>
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                type="username"
+                id="username"
+                name="username"
+                placeholder="Enter your @username"
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Create a password"
+                required
+              />
+            </div>
+
+            <button type="submit" className="btn">
+              Login
+            </button>
+
+            <div className="login-divider text-gray-500 text-center my-1 text-xl">
+              or
+            </div>
+
+            <div className="google-login flex justify-center">
+              <button
+                type="button"
+                onClick={handleGoogleLogin}
+                className="google-btn w-full flex justify-center items-center gap-3 text-[#3c4043] text-xl font-medium 
+                py-2 px-4 rounded-full border border-[#dadce0] bg-white hover:bg-gray-50 
+                hover:shadow-md transition-all duration-200 shadow-md"
+              >
+                <img src="./google.png" alt="Google Logo" className="h-8 w-8" />
+                Sign in with Google
+              </button>
+            </div>
+
+            {Object.keys(errors).length > 0 ? (
+              <div className="error-message">{errors.error}</div>
+            ) : null}
+          </form>
+        </div>
+
+        <div className="form-footer">
+          <p>
+            Don't have an account?{" "}
+            <span
+              className="link text-blue-600 font-bold cursor-pointer"
+              onClick={() => navigate("/signup")}
+            >
+              Register here
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,7 +1,149 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./Form.css";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const navigate = useNavigate();
+
+  const handeleSubmit = (e) => {
+    e.preventDefault();
+    const user = {
+      firstname,
+      lastname,
+      username,
+      email,
+      password,
+    };
+    axios
+      .post(`${import.meta.env.VITE_API_URL}/api/users/register`, user)
+      .then((res) => {
+        console.log(res);
+        if (res.status === 201) {
+          navigate("/login");
+        }
+      })
+      .catch((err) => {
+        const errorMessage =
+          err.response?.data?.errors?.[0]?.msg ||
+          err.response?.data?.message ||
+          "Could not register";
+        console.log(err);
+        setErrors({ error: errorMessage });
+      });
+    setFirstname("");
+    setLastname("");
+    setUsername("");
+    setEmail("");
+    setPassword("");
+  };
+  return (
+    <div className="container-body">
+      <div className="container">
+        <div className="form-header">
+          <h2>
+            Register to <span className="text-[#85def8]">Vibe</span>
+            <span className="text-[#9de5b8]">Chat</span>
+            <img
+              className="w-8 h-8 ml-3 inline-block"
+              src="./chat.png"
+              alt=""
+            />
+            <p className="welcome-message text-sm">
+              Welcome! Please fill in the details to get started.
+            </p>
+          </h2>
+
+          <form onSubmit={handeleSubmit}>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <div className="name-inputs">
+                <input
+                  value={firstname}
+                  onChange={(e) => setFirstname(e.target.value)}
+                  type="text"
+                  id="firstname"
+                  name="firstname"
+                  placeholder="First Name"
+                  required
+                />
+                <input
+                  value={lastname}
+                  onChange={(e) => setLastname(e.target.value)}
+                  type="text"
+                  id="lastname"
+                  name="lastname"
+                  placeholder="Last Name"
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                type="username"
+                id="username"
+                name="username"
+                placeholder="Enter your username"
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Enter your email"
+                required
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Create a password"
+                required
+              />
+            </div>
+
+            <button type="submit" className="btn">
+              Sign Up
+            </button>
+
+            {Object.keys(errors).length > 0 ? (
+              <div className="error-message">{errors.error}</div>
+            ) : null}
+          </form>
+        </div>
+        <div className="form-footer">
+          <p>
+            Already have an account?{" "}
+            <span className="link" onClick={() => navigate("/login")}>
+              Login here
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
