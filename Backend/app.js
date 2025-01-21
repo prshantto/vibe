54543a69e85857cd7f663e3a6bb40399792f0485
prashant <prashanttt001@gmail.com>
2025-01-21 22:03:06 +0530

    changed backend file type to module

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,16 @@
-const express = require("express");
+import express from "express";
 const app = express();
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World! This is the backend of the Vibe application.");
 });
 
-module.exports = app;
+export default app;
--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,5 @@
-const http = require("http");
-const app = require("./app");
-const dotenv = require("dotenv");
-
-dotenv.config();
+import http from "http";
+import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
