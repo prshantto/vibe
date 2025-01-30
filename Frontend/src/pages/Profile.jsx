b3b569982ddc02e8b384c9da3e63d0ff70eeca39
prashant <prashanttt001@gmail.com>
2025-01-30 21:33:32 +0530

    profile page modification

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -12,6 +12,7 @@ const Profile = () => {
   lastSignInTimeString.pop();
   lastSignInTimeString.shift();
   const lastSignInTime = lastSignInTimeString.join(" ");
+  const [isOn, setIsOn] = useState(false);
 
   useEffect(() => {
     const fetchProfileData = async (user) => {
@@ -37,9 +38,9 @@ const Profile = () => {
     <>
       <NavBar />
 
-      <div className="w-screen h-[90vh] flex justify-center items-center flex-col gap-5 px-4 md:px-0">
+      <div className="w-screen h-[90vh] flex justify-center items-center flex-col md:flex-row gap-5 px-4 md:px-0">
         <div className="w-full md:w-[40%] h-auto md:h-[60%] bg-gray-300 rounded-xl flex items-center flex-col p-4 md:p-0">
-          <h1 className="text-2xl font-bold my-3">Profile</h1>
+          <h1 className="text-2xl font-bold my-5">Profile</h1>
           <img
             src={
               auth.currentUser.photoURL ||
@@ -61,27 +62,40 @@ const Profile = () => {
           </div>
         </div>
 
-        <div className="w-full md:w-[40%] h-auto md:h-[30%] bg-gray-300 rounded-xl flex items-center flex-col p-4 md:p-0">
-          <h1 className="text-2xl font-bold my-3">Account Information</h1>
-          <div className="w-full text-lg md:text-xl px-2 md:px-3 font-semibold mt-3">
-            <div className="flex justify-between">
-              <h2 className="font-bold">Last Signin</h2>
-              <p>{lastSignInTime}</p>
-            </div>
-            <div className="flex justify-between mt-3">
-              <h2 className="font-bold">Member Since</h2>
-              <p>
-                {creationTime[1] +
-                  " " +
-                  creationTime[2] +
-                  " " +
-                  creationTime[3]}
-              </p>
-            </div>
-            <div className="flex justify-between mt-3">
-              <h2 className="font-bold">Account Status</h2>
-              <p>Active</p>
-            </div>
+        <div className="w-full md:w-[40%] h-auto md:h-[60%] bg-gray-300 rounded-xl flex items-center flex-col p-4 md:p-0">
+          <h1 className="text-2xl font-bold my-5">Account Information</h1>
+          <div className="w-full text-lg md:text-xl px-2 md:px-3 mt-3">
+            <h2 className="font-bold mb-1">Last Signin</h2>
+            <p className="border-2 border-black rounded-lg py-1.5 px-1 mb-4">
+              {lastSignInTime}
+            </p>
+            <h2 className="font-bold mb-1">Member Since</h2>
+            <p className="border-2 border-black rounded-lg py-1.5 px-1 mb-4">
+              {creationTime[1] + " " + creationTime[2] + " " + creationTime[3]}
+            </p>
+            <h2 className="font-bold mb-1">Account Status</h2>
+            <p className="border-2 border-black rounded-lg py-1.5 px-1 mb-4 flex justify-between">
+              {isOn ? <span>Online</span> : <span>Offline</span>}
+              <label className="relative inline-block w-16 h-8">
+                <input
+                  type="checkbox"
+                  checked={isOn}
+                  onChange={() => setIsOn(!isOn)}
+                  className="hidden"
+                />
+                <span
+                  className={`block cursor-pointer pt-1 bg-gray-300 rounded-full w-full h-full transition-colors ${
+                    isOn ? "bg-green-500" : "bg-red-500"
+                  }`}
+                >
+                  <span
+                    className={`block w-6 h-6 bg-white rounded-full transition-transform ${
+                      isOn ? "transform translate-x-10" : ""
+                    }`}
+                  ></span>
+                </span>
+              </label>
+            </p>
           </div>
         </div>
       </div>
