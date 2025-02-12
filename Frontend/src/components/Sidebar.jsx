81e465e99b1bc77057dc86f1f745477f895f4283
prashant <prashanttt001@gmail.com>
2025-02-12 16:29:16 +0530

    creating sidebar component

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -1,4 +1,9 @@
-import React from "react";
+import useEffect from "react";
+import axios from "axios";
+
+useEffect(() => {
+  axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/users`);
+}, []);
 
 const Sidebar = () => {
   return <div className="w-[25%] bg-red-500">Sidebar</div>;
